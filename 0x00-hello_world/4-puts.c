67fecd3414fc7d24b34f3f8ed3d0ca2ec4e39c96
newtlub <mcheatham283@gmail.com>
2022-09-19 18:55:57 -0700

    ...

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/* This script is setting char to my msg, then puts is printing it. Return ends program */
 int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";
