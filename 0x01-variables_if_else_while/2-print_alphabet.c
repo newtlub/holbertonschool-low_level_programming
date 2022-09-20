4257d0df3cb93a09a3c4263ac23936c680880a20
newtlub <mcheatham283@gmail.com>
2022-09-20 12:05:13 -0700

    ...

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *	main - prints out the alphabet in lower case
+ *	only using a for loop and the putchar function
+ *
+ *	Return: always ends program
+ */
+
+int main(void)
+{
+	for (int x = 'A'; x <= 'Z'; x++)
+	{
+	int lower_x = tolower(x);
+	putchar(lower_x)
+	}
+	return (0);
+}
