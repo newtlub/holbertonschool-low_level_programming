63e6b34c0c76335d70df13385911a6e9e3c635fb
newtlub <mcheatham283@gmail.com>
2022-09-22 14:20:54 -0700

    ...

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *	main - printing all single digits possible in base10
+ *
+ *
+ *	Return: on success (0)
+ */
+
+int main(void);
+{
+	
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar("%d", i);
+	}
+	putchar('\n');
+	return (0);
+
+}
