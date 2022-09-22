8a82f379449ba6bdb9db78169833892137c54190
newtlub <mcheatham283@gmail.com>
2022-09-22 12:33:01 -0700

    ...

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,12 @@
 
 int main(void)
 {
-	for (int x = 'A'; x <= 'Z'; x++)
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-	int lower_x = tolower(x);
-
-	putchar(lower_x)
+	putchar(ch);
 	}
+	putchar('\n');
 	return (0);
 }
