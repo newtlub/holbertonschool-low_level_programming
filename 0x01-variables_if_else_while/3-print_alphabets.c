0b69746bacbb3930c89381e275af0663270efb4a
newtlub <mcheatham283@gmail.com>
2022-09-22 12:45:41 -0700

    ...

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.io>
+
+/**	main - printing out alphabet in lowercase followed by uppercase
+ *
+ *	Return: on success (0)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
