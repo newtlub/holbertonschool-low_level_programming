9284d40d38d8c8eecd77ea4b17b99375b4d8e0d1
newtlub <mcheatham283@gmail.com>
2022-09-24 23:11:34 -0700

    ...

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,22 @@
  *	main - prints out the alphabet using the print_alphabet
  *	function
  *
+ *	print_alphabet - prints alphabet in lowercase
+ *
  *	Return: on success (0)
  */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	return;
+}
 
 int main(void)
 {
