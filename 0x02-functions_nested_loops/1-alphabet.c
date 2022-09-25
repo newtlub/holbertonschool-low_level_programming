7786ffd4b2eebabac5073076f0abd6f11ea41daa
newtlub <mcheatham283@gmail.com>
2022-09-24 23:00:21 -0700

    ...

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+ *	main - prints out the alphabet using the print_alphabet
+ *	function
+ *
+ *	Return: on success (0)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
