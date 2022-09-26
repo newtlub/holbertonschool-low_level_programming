bb45a5d01f35bd5fec1b51d9600aebddc7b84123
newtlub <mcheatham283@gmail.com>
2022-09-26 08:35:37 -0700

    ...

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - determines if the character is a lowercase alpha
+ * character
+ *
+ * Return: if lowercase return (1) otherwise return (0)
+ */
+
+int _islower(int c);
+{
+	int c;
+
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
