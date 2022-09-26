9bc438348f2211326c4e21ed641a28c8753daa0a
newtlub <mcheatham283@gmail.com>
2022-09-26 12:44:59 -0700

    ...

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,10 +13,7 @@ void jack_bauer(void)
 	int h;
 	int m;
 
-	h = 0;
-	m = 0;
-
-	while (h < 24)
+	for (h = 0;h < 24; h++)
 	{
 		while (m < 60)
 		{
@@ -28,6 +25,7 @@ void jack_bauer(void)
 		_putchar('\n');
 		m++;
 		}
+	m = 0;
 	h++;
 	}
 
