7bdd13e8d19f145bd79670dc5ff01aae84af3b52
newtlub <mcheatham283@gmail.com>
2022-09-26 12:33:30 -0700

    ...

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer: A function the prints from 00:00 to 23:59
+ *
+ *
+ * Return: (0) as command runs it prints out
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+	
+	while (h < 24)
+	{
+		while (m < 60)
+		{
+		_putchar((h % 10) + '0');
+		_putchar((h % 10) + '0');
+		_putchar(':');
+		_putchar((m % 10) + '0');
+		_putchar((m % 10) + '0');
+		_putchar('\n')
+		m++;
+		}
+	h++;
+	}	
+
+}
