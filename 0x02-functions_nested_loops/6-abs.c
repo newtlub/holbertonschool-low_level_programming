b5ef37d912928a4c49da110e2703e2735494a3ce
newtlub <mcheatham283@gmail.com>
2022-09-26 09:59:37 -0700

    ...

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
+ * _abs - find the absolute value of the int provided
  *
  *
- *
- *
- *
+ * @n: integer provided to convert from negative to positive.
+ * Return: (0)
  */
 
 int _abs(int n)
