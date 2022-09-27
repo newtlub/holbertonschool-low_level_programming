c37d66ca22b4a86ff6bf6d794aefb1b46371a784
newtlub <mcheatham283@gmail.com>
2022-09-27 16:15:11 -0700

    ...

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - prints the numbers 0 - 14 in ten lines
+ *
+ * @i - counts from 0-14 printing each
+ * @j - runs the printing of 0-14 ten times
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 10)
+				_putchar('1');
+		}
+			_putchar((i) % 10 + '0');
+		i = 0;
+		_putchar('\n');
+	}
+}
