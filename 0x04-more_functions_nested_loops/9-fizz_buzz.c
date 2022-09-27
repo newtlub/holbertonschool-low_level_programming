2e55f1b5a118ed9f4d5cb3642ef7ce7004ff1069
newtlub <mcheatham283@gmail.com>
2022-09-27 00:19:53 -0700

    ...

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,21 @@
 int main(void)
 {
 	int i;
+	int s;
+	s = 0;
 
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if ((i % 3) == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if ((i % 5) == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
 			printf("%d", i);
+	if (i != 100)
+		printf(" ");
 	}
 return (0);
 }
