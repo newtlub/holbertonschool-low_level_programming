fbe7dfd5cce0daa4336a07ddfb217f9a9aba3799
Matthew Cheatham <mcheatham283@gmail.com>
2022-09-26 20:44:36 -0500

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,57 +1,37 @@
 #include "main.h"
-
 /**
- * times_table - prints out a multiplcation table up until 9
+ * times_table - Prints the 9 times table
  *
- * @a - variable used to track the rows of lines & use in arithmatic 
- * @b - variable to track columns & use in arithmatic
- * @c - used to store result of arithmatic and print based on value
- * Return: function prints table as it runs (0)
+ * Return: no return
  */
-
 void times_table(void)
 {
-	int a;
-	int b;
-	int c;
-
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			c = (a * b);
-			if (c == 0)
-			{
-				if (a > 0)
-				{
-					_putchar(' ');	
-				}
-				else
-				{
-					for (c = 0; c < 1; c++)
-					{
-					_putchar('0' + c);
-					_putchar(',');
-					_putchar(' ');
-					}
-					c = 0;
-				}
-			}
-			else if (c <= 9)
-			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(c + '0');
-			}
-			else if (c > 9)
-			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
-			}
-		}
-	_putchar('\n');
-	}
+int a, r;
+for (a = 0; a <= 9; a++)
+{
+for (r = 0; r <= 9; r++)
+{
+if (a * r > 9)
+{
+if (r != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+_putchar((a * r) / 10 + '0');
+_putchar((a * r) % 10 + '0');
+}
+else
+{
+if (r != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+_putchar((a * r) + '0');
+}
+}
+_putchar('\n');
+}
 }
