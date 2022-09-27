0c0594eab81b236f5f8b5f0528dd100b8ed35e56
newtlub <mcheatham283@gmail.com>
2022-09-26 18:15:19 -0700

    ...

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,9 +3,9 @@
 /**
  * times_table - prints out a multiplcation table up until 9
  *
- * @a - tracking loops through till 9 & prints first 3 characters
- * @b - prints out second digit if applicable and prints the math
- * @c - variable used to calculate the math
+ * @a - variable used to track the rows of lines & use in arithmatic 
+ * @b - variable to track columns & use in arithmatic
+ * @c - used to store result of arithmatic and print based on value
  * Return: function prints table as it runs (0)
  */
 
@@ -15,23 +15,23 @@ void times_table(void)
 	int b;
 	int c;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 1; b < 10; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = (b * a);
 			if (c == 0)
 			{
 				_putchar('0' + c);
 			}
-			else if (c < 10)
+			else if (c <= 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(c + '0');
 			}
-			else if (c < 10)
+			else if (c > 9)
 			{
 				_putchar(',');
 				_putchar(' ');
