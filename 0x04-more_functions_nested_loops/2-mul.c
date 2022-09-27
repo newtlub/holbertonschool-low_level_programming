5cff5d18f9a5313d7f1ab84d469a225973072ad9
newtlub <mcheatham283@gmail.com>
2022-09-27 13:38:21 -0700

    ...

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies the two provided int
+ *
+ * @a: first int to use in function
+ * @b: second int to use in function
+ * Return: always (0)
+ */
+
+int mul(int a, int b)
+{
+	_putchar((a * b) + '0');
+	return (0);
+}
