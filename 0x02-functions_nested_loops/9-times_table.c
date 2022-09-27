077d133d512d105338b0c18018b3351281832f03
newtlub <mcheatham283@gmail.com>
2022-09-26 17:27:26 -0700

    ...

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - prints out a multiplcation table up until 9
+ *
+ * @a - tracking loops through till 9 & prints first 3 characters
+ * @b - prints out second digit if applicable and prints the math
+ * @c - variable used to calculate the math
+ * Return: function prints table as it runs (0)
+ */
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a < 10; a++;)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (b = 1; j < 10; b++)
+		{
+			c = (a * b);
+			if (c < 10)
+			{
+			_putchar('_');
+			}
+			else
+			{
+			_putchar((c / 10)+ '0');
+			}
+			_putchar((c % 10)+ '0');
+		}
+		a++;
+}
+
+
+
+	
+}
