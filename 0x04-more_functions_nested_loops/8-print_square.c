38ca4b2fe85dc0d4e08704312803c4690be0848c
newtlub <mcheatham283@gmail.com>
2022-09-27 20:13:26 -0700

    ...

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - prints out a square to the dimensions from provided int
+ *
+ * @size: provided value that determines the size of the square
+ */
+
+void print_square(int size)
+{
+	int i, x;
+
+	i = n;
+	if (n > 0)
+	{
+		for (i = n; i > 0; i--)
+		{
+			x = n;
+			while (x > 0)
+			{
+				-putchar('#');
+				x--;
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
