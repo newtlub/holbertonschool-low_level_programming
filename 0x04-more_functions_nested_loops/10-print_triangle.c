c30c6158968048ac292bafd3be5af57902fa4b3d
newtlub <mcheatham283@gmail.com>
2022-09-27 21:41:04 -0700

    ...

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,10 +4,6 @@
  * print_triangle - prints a triangle with the provided size parameters
  *
  * @size: size int where user provides size of the triangle
- * @i: tracks the rows of the triangle as its printing
- * @x: prints the empty space before the body of the triangle
- * @h: prints the hashtags making up the body of the triangle
- * @s: placeholder for the provided size int
  */
 
 void print_triangle(int size)
