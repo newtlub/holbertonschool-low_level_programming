4b5998f1b5656a2af6c17f2d4a858458c58c488e
Matthew Cheatham <mcheatham283@gmail.com>
2022-09-28 22:11:32 -0500

    Update 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ *
+ * @s: string input
+ */
+void rev_string(char *s)
+{
+	int a = 0;
+	char charArr[999];
+	int b = 0;
+
+	while (*(s + a) != '\0')
+	{
+		charArr[a] = *(s + a);
+		a++;
+	}
+	a--;
+	while (a > -1)
+	{
+		*(s + a) = charArr[b];
+		a--;
+		b++;
+	}
+}
