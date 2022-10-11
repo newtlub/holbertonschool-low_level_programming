e9123ebe049ae02e12fd50e34e980f3fcf960223
newtlub <mcheatham283@gmail.com>
2022-10-11 10:00:37 -0700

    ...

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,8 +9,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	_print_rev_recursion(*s + 1);
-	_putchar("%s", s);
-
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
