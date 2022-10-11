e561f08c2aa4499a6c186bfb225b82b0da5d0376
newtlub <mcheatham283@gmail.com>
2022-10-11 10:06:09 -0700

    ...

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string using recursion
+ *
+ * @s: String provided for function
+ * Return: (0) if successful
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_puts_recursion(s - 1);
+		_putchar(*s);
+	}
+
+}
