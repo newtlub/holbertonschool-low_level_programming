50806bb287c8fc89cba3e434f5007c9348069a82
newtlub <mcheatham283@gmail.com>
2022-10-11 17:52:54 -0700

    ...

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ *
+ * @argc: amount of arguments in cmd line when exe is run
+ * @argv: array of strings of each argument in cmd line
+ * Return: always (0)
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d", argc);
+	if (argc || argv)
+	{
+		/*yuh*/
+	}
+	return (0);
+}
