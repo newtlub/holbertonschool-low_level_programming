db3dfc5854d421fe5d01a5a7c16b9a9699afa0df
newtlub <mcheatham283@gmail.com>
2022-10-11 17:35:30 -0700

    ...

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints out the programs name
+ *
+ * @argc: amount of command arguments enter in cmd line
+ * @argv: array of strings entered in the cmd line
+ * Return: always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(*argv[0]);
+	if (argc || argv)
+	{
+		/* BETTY SMELLY */
+	}
+	return (0);
+}
