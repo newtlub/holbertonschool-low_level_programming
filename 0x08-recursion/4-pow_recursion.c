f7723974939fb9eb75dbbba1c8991fd1557737e5
newtlub <mcheatham283@gmail.com>
2022-10-12 20:30:05 -0700

    ...

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
  *
  * @x: value x
  * @y: to the power of y
- * Returns: the value of x raised to the power of y
+ * Return: the value of x raised to the power of y
  */
 
 int _pow_recursion(int x, int y)
