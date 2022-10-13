126caf158a7bd9d05b0850145935d9f42f455323
newtlub <mcheatham283@gmail.com>
2022-10-12 20:43:54 -0700

    ...

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**********/
+
+int root(int n, int rt);
+{
+	if ((rt * rt) == num)
+		return(rt);
+	if (rt == n / 2)
+		return(-1);
+	return (root(n, rt + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	int rt = n;
+
+	if (n < 0)
+		return (-1);
+	return (root(n, rt));	
+}
