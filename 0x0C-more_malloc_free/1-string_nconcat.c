458691949aef2427fa4e526e19df0ee21a69e833
newtlub <mcheatham283@gmail.com>
2022-10-21 16:47:44 -0700

    ...

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int a, b, i;
+	char *ptr;
+	
+	a = _strlen(s1);
+	b = _strlen(s2);
+
+	if (n >= b)
+		then use entire s2 string;
+
+	ptr = (char *)malloc(a + b);
+
+	for (i = 0; i == n; i++)
+	{
+		if ()
+		ptr[i] = 
+	}
+	if (ptr == NULL)
+		return (NULL);	
+	return(ptr);
+
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#define DOG_H
+#ifndef DOG_H
+
+/**
+ * struct dog - a struct storing name age and owner
+ *
+ * @name: name of dog
+ * @age: age of the puppo
+ * @owner: owner of the doggy
+ *
+ * Description: This is a struct of two strings and a float
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
