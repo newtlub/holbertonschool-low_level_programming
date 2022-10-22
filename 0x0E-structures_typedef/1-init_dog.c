3242428db4634209abbd5fc1bdb57865d9725d66
newtlub <mcheatham283@gmail.com>
2022-10-21 18:10:39 -0700

    ...

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,12 @@
 #include "dog.h"
 
 /**
+ * init_dog - initialize a new var of struct dog with provided vars
  *
- *
- *
- *
- *
+ * @d: name of the new instance of the struct type
+ * @name: name to store in the new struct
+ * @age: age float of dog
+ * @owner: string of owner name
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
