04dfb8409babd94234c4e0e32074094f3649500f
newtlub <mcheatham283@gmail.com>
2022-10-25 15:43:34 -0700

    ...

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,12 +1,18 @@
 #include "variadic_functions.h"
 
-/******/
+/**
+ * print_numbers - prints out a variable list of numbers separated by a string
+ *
+ * @separator: string to print between each int
+ * @n: number of numbers to be printed out
+ */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 
 	va_list args;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
